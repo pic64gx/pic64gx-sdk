b60d1bc15b78bf0442e1d8ae7aa12b4e3c509812
Conor Paxton <conor.paxton@microchip.com>
2024-10-19 00:56:37 +0100

    tools: remove redundant project templates
    the generic templates for Microchip device families are not needed.
    
    Co-authored-by: Emma Davenport <emma.davenport@microchip.com>
    Signed-off-by: Conor Paxton <conor.paxton@microchip.com>

--- tools/templates/projects/miv/src/application/main.c
+++
@@ -1,9 +0,0 @@
-#include "miv_rv32_hal/miv_rv32_hal.h"
-#include "fpga_design_config/fpga_design_config.h"
-
-int main(void) {
-    while(1u) {
-
-    }
-    return 0u;
-}
--- tools/templates/projects/mps/src/application/main.c
+++
@@ -1,9 +0,0 @@
-#include "hal/hal.h"
-#include "monp_hal/monp_hal.h"
-
-int main(void) {
-    while(1u) {
-
-    }
-    return 0u;
-}
