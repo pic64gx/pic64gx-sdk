1ffdaaa9111cf73b6bd8af77d555faba6fdce8ae
Conor Paxton <conor.paxton@microchip.com>
2024-10-21 17:49:41 +0100

    cmake: remove redundant microchip device families from common
    A common CMake framework is used to compile bare-metal software for all
    of Microchip's RISC-V device familes. Strip out whats not needed for
    PIC64GX
    
    Signed-off-by: Conor Paxton <conor.paxton@microchip.com>

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -1,14 +1,5 @@
 cmake_minimum_required(VERSION 3.27.1)
 
-# when cmake functions are overridden the original version of the function can
-# be called by prepending the function call with an underscore
-# override the cmake project() function to a noop
-
-# Trick to temporarily redefine project(). When functions are overridden in 
-# CMake, the originals can still be accessed using an underscore prefixed 
-# function of the same name. The following lines make sure that __project  calls
-# the original project(). See https://cmake.org/pipermail/cmake/2015-October/061751.html.
-
 macro(sdk_project project_name)
 project(${project_name} C ASM CXX)
 
@@ -70,29 +61,15 @@ foreach(item ${CONFIG})
     cmake_language(EVAL CODE "set (${item})")
 endforeach()
 
+if(EXISTS ${BOARD})
+    file(GLOB BOARD_CONTENTS ${BOARD}/*)
+    file(COPY ${BOARD_CONTENTS} DESTINATION ${CMAKE_BINARY_DIR}/board)
+else()
+    message(FATAL_ERROR "Cannot find board ${BOARD}. Please check that the path to your board is correct")
+endif()
+
 if(DEFINED CONFIG_PLATFORM_MSS)
-    set(CONFIG_PLATFORM mss)
-    set(SYS_PROC rv64imafdc_zicsr)
-    set(MARCH rv64imafdc_zicsr_zifencei)
-    set(MABI lp64d)
-    set(HEX_FORMAT ihex)
-elseif(DEFINED CONFIG_PLATFORM_MIV)
-    set(CONFIG_PLATFORM miv)
-    set(SYS_PROC rv32im_zicsr)
-    set(MARCH rv32im_zicsr_zifencei)
-    set(MABI ilp32)
-    set(HEX_FORMAT ihex)
-elseif(DEFINED CONFIG_PLATFORM_MPS)
-    set(SYS_PROC rv64imafc_zicsr)
-    set(MARCH rv64imafc_zicsr)
-    set(MABI lp64f)
-    set(HEX_FORMAT ihex)
-elseif(DEFINED CONFIG_PLATFORM_VPB)
-    set(CONFIG_PLATFORM vpb)
-    set(SYS_PROC rv64imafc_zicsr)
-    set(MARCH rv64imafc_zicsr_zifencei_zve32f_zfh)
-    set(MABI lp64f)
-    set(HEX_FORMAT verilog)
+    include(${WORKSPACE}/platforms/mss/platform/CMakeLists.txt)
 endif()
 
 message(STATUS "${CONFIG_MARCH} ${CONFIG_MABI} ${CONFIG_SYSTEM_PROCESSOR}")
@@ -133,137 +110,6 @@ file(GLOB_RECURSE MIDDLEWARE ${CMAKE_SOURCE_DIR}/src/middleware/*.c)
 file(GLOB_RECURSE MIDDLEWARE_ASSEM ${CMAKE_SOURCE_DIR}/src/middleware/*.S)
 list(APPEND MIDDLEWARE ${MIDDLEWARE_ASSEM})
 
-if(EXISTS ${BOARD})
-    file(GLOB BOARD_CONTENTS ${BOARD}/*)
-    file(COPY ${BOARD_CONTENTS} DESTINATION ${CMAKE_BINARY_DIR}/board)
-else()
-    message(FATAL_ERROR "Cannot find board ${BOARD}. Please check that the path to your board is correct")
-endif()
-
-# assemble headers and sources based on the chosen platform
-if(DEFINED CONFIG_PLATFORM_MSS)
-    # including the CMakeLists.txt file from platform exposes the MSS_DRIVERS and
-    # MIV_DRIVERS lists to the build system
-    add_subdirectory(${WORKSPACE}/platforms/mss/platform ${CMAKE_BINARY_DIR}/platform)
-
-    set(linker_script_path ${WORKSPACE}/platforms/mss/platform/platform_config_reference/linker/)
-
-    execute_process(
-        COMMAND ${PYTHON_EXECUTABLE} 
-        ${WORKSPACE}/platforms/mss/platform/soc_config_generator/mpfs_configuration_generator.py 
-        ${CMAKE_BINARY_DIR}/board/fpga_design/design_description/ 
-        ${CMAKE_BINARY_DIR}/board
-    )
-
-    list(APPEND headers
-        ${CMAKE_SOURCE_DIR}/src/application/
-        ${WORKSPACE}/platforms/mss/platform
-        ${WORKSPACE}/platforms/mss/platform/mpfs_hal/
-        ${CMAKE_BINARY_DIR}/board/
-        ${CMAKE_BINARY_DIR}/board/fpga_design_config/
-        ${CMAKE_BINARY_DIR}/board/platform_config/
-        ${WORKSPACE}/platforms/mss/platform/platform_config_reference/
-        ${CMAKE_BINARY_DIR}
-    )
-
-    if(${CONFIG_MEMORY} MATCHES "ddr")
-        list(APPEND headers
-            ${CMAKE_BINARY_DIR}/board/platform_config_ddr/
-            ${CMAKE_BINARY_DIR}/board/platform_config_ddr/mpfs_hal_config/    
-        )
-    else()
-        list(APPEND headers
-            ${CMAKE_BINARY_DIR}/board/platform_config/
-            ${CMAKE_BINARY_DIR}/board/platform_config/mpfs_hal_config/    
-        )
-    endif()
-
-    set(sources
-        ${HAL}
-        ${MPFS_HAL}
-        ${ASSEM}
-        ${MSS_DRIVERS}
-        ${MIDDLEWARE}
-    )
-
-    if(${CONFIG_MEMORY} MATCHES "envm")
-        set(linker_script ${linker_script_path}/mpfs-envm.ld)
-    elseif(${CONFIG_MEMORY} MATCHES "scratchpad")
-        set(linker_script ${linker_script_path}/mpfs-lim-lma-scratchpad-vma.ld)
-    elseif(${CONFIG_MEMORY} MATCHES "lim")
-        set(linker_script ${linker_script_path}/mpfs-lim.ld)
-    elseif(${CONFIG_MEMORY} MATCHES "ddr")
-        set(linker_script ${linker_script_path}/mpfs-ddr-loaded-by-boot-loader.ld)
-    else()
-        set(linker_script ${linker_script_path}/mpfs-lim.ld)
-    endif()
-
-elseif(DEFINED CONFIG_PLATFORM_MIV)
-    add_subdirectory(${WORKSPACE}/platforms/miv/platform ${CMAKE_BINARY_DIR}/platform)
-
-    set(linker_script_path ${WORKSPACE}/platforms/miv/platform/platform_config_reference/linker/)
-
-    list(APPEND headers
-        ${WORKSPACE}/platforms/miv/platform
-        ${WORKSPACE}/platforms/miv/platform/miv_rv32_hal/
-        ${CMAKE_BINARY_DIR}/board/
-        ${CMAKE_BINARY_DIR}/board/platform_config/
-        ${CMAKE_BINARY_DIR}
-    )
-
-    set(sources
-        ${HAL}
-        ${MIV_RV32_HAL}
-        ${ASSEM}
-        ${MIV_DRIVERS}
-        ${MIDDLEWARE}
-    )
-
-    set(linker_script ${linker_script_path}/miv-rv32-ram.ld)
-
-elseif(DEFINED CONFIG_PLATFORM_MPS)
-    add_subdirectory(${WORKSPACE}/platforms/mps/platform ${CMAKE_BINARY_DIR}/platform)
-
-    set(linker_script_path ${WORKSPACE}/platforms/mps/platform/platform_config_reference/linker/)
-
-    list(APPEND headers
-        ${WORKSPACE}/platforms/mps/platform
-        ${WORKSPACE}/platforms/mps/platform/monp_hal/
-        ${CMAKE_BINARY_DIR}/board/
-        ${CMAKE_BINARY_DIR}
-    )
-
-    set(sources
-        ${HAL}
-        ${MONP_HAL}
-        ${ASSEM}
-        ${MPS_DRIVERS}
-        ${MIDDLEWARE}
-    )
-
-    set(linker_script ${linker_script_path}/monp_hal_ram.ld)
-
-elseif(DEFINED CONFIG_PLATFORM_VPB)
-    add_subdirectory(${WORKSPACE}/platforms/vpb/platform ${CMAKE_BINARY_DIR}/platform)
-
-    set(linker_script ${WORKSPACE}/platforms/vpb/platform/platform_config_reference/linker/vpb_vsram.ld)
-
-    list(APPEND headers
-        ${WORKSPACE}/platforms/vpb/platform
-        ${WORKSPACE}/platforms/vpb/platform/vpb_hal/
-        ${CMAKE_BINARY_DIR}/board/
-        ${CMAKE_BINARY_DIR}
-    )
-
-    set(sources
-        ${HAL}
-        ${VPB_HAL}
-        ${ASSEM}
-        ${VPB_DRIVERS}
-        ${MIDDLEWARE}
-    )
-endif()
-
 if(DEFINED EXTRA_MODULES)
     foreach(MODULE ${EXTRA_MODULES})
         get_filename_component(MODULE_BIN_DIR ${MODULE} NAME)
@@ -271,8 +117,12 @@ if(DEFINED EXTRA_MODULES)
     endforeach()
 endif()
 
-target_include_directories(${PROJECT_NAME} PUBLIC ${headers})
-target_sources(${PROJECT_NAME} PUBLIC ${sources})
+target_include_directories(${PROJECT_NAME} PUBLIC ${PLATFORM_HEADERS})
+target_sources(${PROJECT_NAME} PUBLIC ${PLATFORM_SRC})
+
+if(DEFINED USER_LINKER AND NOT "${USER_LINKER}" STREQUAL "")
+    set(linker_script ${USER_LINKER})
+endif()
 
 execute_process(
     COMMAND ${CMAKE_C_COMPILER}
@@ -299,9 +149,16 @@ target_link_libraries(${PROJECT_NAME} m)
 
 add_custom_command(
         TARGET ${PROJECT_NAME} POST_BUILD
-        COMMAND ${CMAKE_OBJCOPY} -O ${HEX_FORMAT} ${CMAKE_BINARY_DIR}/${PROJECT_NAME}.elf ${CMAKE_BINARY_DIR}/${PROJECT_NAME}.hex
+        COMMAND ${CMAKE_OBJCOPY} -O binary ${CMAKE_BINARY_DIR}/${PROJECT_NAME}.elf ${CMAKE_BINARY_DIR}/${PROJECT_NAME}.bin
         COMMAND ${CMAKE_OBJDUMP} --source --all-headers --demangle --line-numbers --wide ${CMAKE_BINARY_DIR}/${PROJECT_NAME}.elf > ${CMAKE_BINARY_DIR}/${PROJECT_NAME}.lst
         COMMAND ${CMAKE_SIZE} --format=sysv --totals --radix=16 ${CMAKE_BINARY_DIR}/${PROJECT_NAME}.elf
 )
+
+if(CONFIG_GEN_IHEX)
+    add_custom_command(
+        TARGET ${PROJECT_NAME} POST_BUILD
+        COMMAND ${CMAKE_OBJCOPY} -O ihex ${CMAKE_BINARY_DIR}/${PROJECT_NAME}.elf ${CMAKE_BINARY_DIR}/${PROJECT_NAME}.hex
+    )
+endif()
 
 endmacro() # project
